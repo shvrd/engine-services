813ef5743f4bcae3782d35a4745680522bef09b3
TheKatze <TheKatze@users.noreply.github.com>
2019-07-20 19:31:18 +0200

    add picopng dependency

---
+++ src/library/util/png/PicoPNG.h
@@ -0,0 +1,12 @@
+//
+// Created by thekatze on 20/07/2019.
+//
+
+#ifndef ENGINITO_PICOPNG_H
+#define ENGINITO_PICOPNG_H
+
+#include <vector>
+
+extern int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width, unsigned long& image_height, const unsigned char* in_png, size_t in_size, bool convert_to_rgba32 = true);
+
+#endif //ENGINITO_PICOPNG_H
