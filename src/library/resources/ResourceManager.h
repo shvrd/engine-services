26d38a28ac285f6dd7568190c2f9294f84b95573
TheKatze <TheKatze@users.noreply.github.com>
2019-07-21 20:17:37 +0200

    make resourcemanager generic

--- src/library/resources/ResourceManager.h
+++ src/library/resources/ResourceManager.h
@@ -6,17 +6,54 @@
 #define ENGINITO_RESOURCEMANAGER_H
 
 #include <map>
+#include <memory>
 #include <string>
-#include "../types/Texture.h"
 
+template<class Resource>
 class ResourceManager {
-    std::map<std::string, Texture> m_textureMap;
+    std::map<std::string, Resource> m_resourceMap;
 public:
     ResourceManager();
     ~ResourceManager();
 
-    Texture loadTexture(std::string filePath);
+    void add(const std::string& filePath, const Resource& resource);
+    std::shared_ptr<Resource> get(const std::string& filePath) const;
+
+    unsigned int size() const;
+
+    void clear();
 };
+
+template<class Resource>
+ResourceManager<Resource>::ResourceManager()
+    : m_resourceMap() {
+
+}
+
+template<class Resource>
+ResourceManager<Resource>::~ResourceManager() {
+    clear();
+}
+
+template<class Resource>
+void ResourceManager<Resource>::add(const std::string &filePath, const Resource &resource) {
+    m_resourceMap.insert(std::make_pair(filePath, resource));
+}
+
+template<class Resource>
+std::shared_ptr<Resource> ResourceManager<Resource>::get(const std::string &filePath) const {
+    return m_resourceMap.find(filePath);
+}
+
+template<class Resource>
+unsigned int ResourceManager<Resource>::size() const {
+    return m_resourceMap.size();
+}
+
+template<class Resource>
+void ResourceManager<Resource>::clear() {
+    m_resourceMap.clear();
+}
 
 
 #endif //ENGINITO_RESOURCEMANAGER_H
