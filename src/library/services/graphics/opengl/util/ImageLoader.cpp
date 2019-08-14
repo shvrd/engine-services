5ce26eb88fe6e7cd3e55e1b54dbe05741845b5bc
TheKatze <TheKatze@users.noreply.github.com>
2019-08-14 19:28:21 +0200

    upload texture to graphics card

--- src/library/services/graphics/opengl/util/ImageLoader.cpp
+++ src/library/services/graphics/opengl/util/ImageLoader.cpp
@@ -5,8 +5,10 @@
 #include "ImageLoader.h"
 #include "../../../../util/FileUtils.h"
 #include "../../../../util/png/PicoPNG.h"
+#include "../../../../log/Logger.h"
 
 #include <vector>
+#include <GL/glew.h>
 
 std::shared_ptr<Texture> ImageLoader::loadPNG(const std::string &filePath) {
     std::shared_ptr<Texture> texture = std::make_shared<Texture>();
@@ -17,7 +19,29 @@ std::shared_ptr<Texture> ImageLoader::loadPNG(const std::string &filePath) {
 
     std::vector<char> encodedData = FileUtils::readBinaryFile(filePath);
 
-    decodePNG(imageData, width, height, reinterpret_cast<unsigned char*>(encodedData.data()), encodedData.size());
+    if (int errorCode = decodePNG(imageData, width, height, reinterpret_cast<unsigned char*>(encodedData.data()), encodedData.size())) {
+        Logger::warn("decodePNG failed with error code " + std::to_string(errorCode));
+    }
 
-    return std::shared_ptr<Texture>();
+    texture->width = width;
+    texture->height = height;
+
+    glGenTextures(1, &texture->id);
+
+    glBindTexture(GL_TEXTURE_2D, texture->id);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei) width, (GLsizei) height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData.data());
+
+    //TODO: Make this stuff configurable
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return texture;
 }
